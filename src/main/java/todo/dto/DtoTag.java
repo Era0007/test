99c4a9ea365ef667e03d807b1f0d3d3a0b75d2f8
Erasyl <lg.8k.ltre@gmail.com>
2025-12-27 16:59:17 +0500

    День 2: Создал DTO классы и UserMapper

---
+++ src/main/java/todo/dto/DtoTag.java
@@ -0,0 +1,15 @@
+// 3. DtoTag.java
+package todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DtoTag {
+    private Long id;
+    private String name;
+    private String color;
+}
